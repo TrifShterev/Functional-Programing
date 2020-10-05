3f5b52d4b9c0eb50025b2cb8c448f1d4d4678377
TrifShterev <tshterev@yahoo.com>
2020-10-05 16:37:03 +0300

    new task added

---
+++ Predicate for Names/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Predicate_for_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n =int.Parse(Console.ReadLine());
+            var names = Console.ReadLine().Split();
+            Func<int, string> GetName = GetCorectName(n, names);
+
+        }
+        static Func<int,string> GetCorectName (int num,string[] names)
+        {
+            foreach (var name in names)
+            {
+                if (num>= name.Length)
+                {
+                    Console.WriteLine(name); ;
+                }
+                
+            }
+            return null;  
+           
+        }
+       
+    }
+}
